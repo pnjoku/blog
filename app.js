d199322926131ec04488ba91039daf0d30c2dedb
Brandon Taylor <alsoicode@gmail.com>
2015-11-26 17:58:11 -0500

    Removed inline handler for posts. Added comments.

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var nconf = require('nconf');
 var db = require('./db');
 var path = require('path');
 var app = express();
-var fs = require('fs');
 var nunjucks = require('nunjucks');
 
 var heartbeat = require('./routes/heartbeat'),
@@ -13,6 +12,8 @@ var heartbeat = require('./routes/heartbeat'),
     blog = require('./routes/blog'),
     blogRouter = express.Router();
 
+
+// Templating
 nunjucks.configure('templates', {
   autoescape: true,
   express: app
@@ -23,25 +24,21 @@ if (config.environment !== 'production') {
     app.use(express.static(path.join(__dirname, 'static_assets')));
 }
 
+// Middleware
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/posts', function(req, res)
-{
-  mongoose.model('Blog').find(function(err, blogs){
-    if(err) console.log(err);
-    res.send(blogs);
-  })
-})
-
+// Routes
 heartbeatRouter.get('/heartbeat', heartbeat.index);
 app.use('/', heartbeatRouter);
 
 blogRouter.get('/email', blog.getEmail);
 blogRouter.post('/email', blog.sendEmail);
 blogRouter.get('/about', blog.about);
+blogRouter.get('/posts', blog.posts);
 blogRouter.get('/', blog.index);
 app.use('/', blogRouter);
 
+// Server
 var server = app.listen(config.get('express:port'), function() {
   console.log('%s starting up on port %s using the %s environment', config.get('application:name'), config.get('express:port'), config.environment);
 });
